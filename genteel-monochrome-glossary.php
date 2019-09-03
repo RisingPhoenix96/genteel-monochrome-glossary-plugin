6aaa06b373d8ee831f80e8a47ddb9547938b397d
Lame Moatshe <lame@castlegateit.co.uk>
2019-09-03 13:01:40 +0100

    adding plugin description

---
+++ genteel-monochrome-glossary.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Genteel Monochome Glossary
+ * Plugin URI: https://lgm.design/
+ * Description: A glossary page plugin for the Genteel Monochrome theme.
+ * Version: 1.0
+ * Author: LGM
+ * Author URI: https://lgm.design/about/
+ * Text Domain: /
+ * Contributors: LGM
+ */
